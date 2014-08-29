c4e82b43bf34efcd07bd4ad07baec2873e158bb4
Przemysław Palak <ppalak@pzu.pl>
2014-08-29 15:58:11 +0200

    Service

--- app/app.js
+++ app/app.js
@@ -5,11 +5,27 @@
 
 var app = angular.module('tutorial', []);
 
-app.controller('ClientController', function ($scope) {
+app.controller('ClientController', function ($scope, ClientService) {
     $scope.message = '';
 
     $scope.submitClient = function(){
+        var client  = {
+            name : $scope.name,
+            surname: $scope.surname,
+            age: $scope.age
+        };
+        ClientService.submitClient(client);
         $scope.message = 'Submited client: '+$scope.name+' '+$scope.surname;
+    }
+});
+
+app.service('ClientService', function(){
+    var clients = [];
+    var submitClient = function(client){
+        clients.push(client);
+    };
+    return{
+        submitClient: submitClient
     }
 });
 
