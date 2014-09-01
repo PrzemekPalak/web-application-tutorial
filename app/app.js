70b7bf5af1732c6a5c0fb20348276d5e1a403ac4
Przemysław Palak <ppalak@pzu.pl>
2014-09-01 10:26:28 +0200

    routeProvider

--- app/app.js
+++ app/app.js
@@ -3,28 +3,35 @@
  */
 "use strict";
 
-var app = angular.module('tutorial', ['controllers']);
+var app = angular.module('tutorial', ['ngRoute', 'controllers']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/client', {templateUrl: 'partials/client.html'})
+        .otherwise({redirectTo: '/client'});
+});
 
 var controllers = angular.module('controllers', ['services']);
 
 controllers.controller('ClientController', function ($scope, ClientService) {
     $scope.message = '';
 
-    $scope.submitClient = function(){
-        var client  = {
-            name : $scope.name,
+    $scope.submitClient = function () {
+        var client = {
+            name: $scope.name,
             surname: $scope.surname,
             age: $scope.age
         };
         ClientService.submitClient(client);
-        $scope.message = 'Submited client: '+$scope.name+' '+$scope.surname;
+        $scope.message = 'Submited client: ' + $scope.name + ' ' + $scope.surname;
     }
 });
 
 var services = angular.module('services', []);
-services.service('ClientService', function($log){
+
+services.service('ClientService', function ($log) {
     var clients = [];
-    var submitClient = function(client){
+    var submitClient = function (client) {
         $log.info('client submited');
         clients.push(client);
     };
