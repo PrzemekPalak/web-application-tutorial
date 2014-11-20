602e5db10da3a1fa590360afdd20125d3aa45992
Przemysław Palak <ppalak@pzu.pl>
2014-11-20 13:32:53 +0100

    maven project

---
+++ env.bat
@@ -0,0 +1,3 @@
+set path=c:\Program Files\Java\jdk1.8.0\bin;c:\tools\apache-maven-3.2.1\bin;%path%
+set JAVA_HOME=c:\Program Files\Java\jdk1.8.0
+set M2_HOME=c:\tools\apache-maven-3.2.1
---
+++ src/main/java/pl/palak/webapptutorial/vertx/App.java
@@ -0,0 +1,10 @@
+package pl.palak.webapptutorial.vertx;
+
+/**
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
