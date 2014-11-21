87d7aeb83094ec0f7ff376d2d0c497803eda1a7a
Przemysław Palak <ppalak@pzu.pl>
2014-11-21 09:54:22 +0100

    evenbus - send

--- src/main/java/pl/palak/webapptutorial/vertx/WebServer.java
+++ src/main/java/pl/palak/webapptutorial/vertx/WebServer.java
@@ -1,9 +1,11 @@
 package pl.palak.webapptutorial.vertx;
 
 import org.vertx.java.core.Handler;
+import org.vertx.java.core.eventbus.EventBus;
 import org.vertx.java.core.http.HttpServer;
 import org.vertx.java.core.http.HttpServerRequest;
 import org.vertx.java.core.http.RouteMatcher;
+import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.platform.Verticle;
 
 /**
@@ -13,13 +15,23 @@ public class WebServer extends Verticle {
 
     @Override
     public void start() {
+        container.logger().info("Starting web server");
         HttpServer httpServer = vertx.createHttpServer();
 
         RouteMatcher routeMatcher = new RouteMatcher();
 
+        final EventBus eventBus = vertx.eventBus();
+
         routeMatcher.all("/api/ping", new Handler<HttpServerRequest>() {
             @Override
             public void handle(HttpServerRequest httpServerRequest) {
+                container.logger().info("ping");
+
+                JsonObject msg = new JsonObject();
+                msg.putString("msg", "ping");
+
+                eventBus.send("pong", msg);
+
                 httpServerRequest.response().end("hello api!");
             }
         });
@@ -35,6 +47,7 @@ public class WebServer extends Verticle {
         httpServer.requestHandler(routeMatcher);
 
         httpServer.listen(8080);
+        container.logger().info("Web server started");
     }
 
 }
